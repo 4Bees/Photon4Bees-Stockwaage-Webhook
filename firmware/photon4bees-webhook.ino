4dd2118bc36841366ae8258a1d491310dfef5916
DieterMetzler <d.metzler@summsumm.com>
2017-04-18 20:52:36 +0200

    Piettetech_DHT Library

--- firmware/photon4bees-webhook.ino
+++ firmware/photon4bees-webhook.ino
@@ -6,6 +6,9 @@
 #include "Adafruit_DHT.h"
 #include "SparkFunMAX17043.h" // Include the SparkFun MAX17043 library
 
+//Piettetech_DHT
+
+
 PRODUCT_ID(1926); // replace by your product ID
 PRODUCT_VERSION(2); // increment each time you upload to the console
 
